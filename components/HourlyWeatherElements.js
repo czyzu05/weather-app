10aa12dca37061cc26f960abafd8eea80ee88e67
czyzu05 <damian.czyz1998@gmail.com>
2020-12-01 18:59:04 +0100

    display elements in FlatList

--- components/HourlyWeatherElements.js
+++ components/HourlyWeatherElements.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
-import { ScrollView, View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import { WeatherContext } from "../WeatherContext";
 import HourlyElement from "./HourlyElement";
 
@@ -7,14 +8,12 @@ const HourlyWeatherElements = () => {
   const { hourlyWeather } = useContext(WeatherContext);
 
   if (hourlyWeather) {
-    const elements = hourlyWeather.hourly.map(item => (
-      <HourlyElement key={item.dt} {...item} />
-    ));
-
     return (
-      <ScrollView>
-        <View style={styles.container}>{elements}</View>
-      </ScrollView>
+      <FlatList
+        data={hourlyWeather.hourly}
+        renderItem={({ item }) => <HourlyElement {...item} key={item.dt} />}
+        horizontal={true}
+      />
     );
   } else {
     return <Text>Error</Text>;
